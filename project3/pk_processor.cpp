ffe59f89405d21e753fd214b900f97a98682f94c
Riley Miller <rmllr000@gmail.com>
2019-12-01 08:38:57 -0700

    added base outline and extraction

--- project3/pk_processor.cpp
+++ project3/pk_processor.cpp
@@ -10,6 +10,22 @@
 // *  it will originate here). The function will be called once for every
 // *  packet in the savefile.
 // ****************************************************************************
+
+// Network Layer constants
+const int ETHERNET_1_THRESH = 1500;
+const int IP_V_4 = 2048;
+const int ARP = 2054;
+const int IP_V_6 = 34525;
+
+const int IP_OFFSET = 14;
+
+// Transport Layer constants
+const int ICMP = 1;
+const int TCP = 6;
+const int UDP = 17;
+
+const int TRANSPORT_LAYER_OFFSET = 35;
+
 void pk_processor(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
 
     resultsC* results = (resultsC*)user;
@@ -24,20 +40,47 @@ void pk_processor(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *
     // * Process the link layer header
     // *  Hint -> use the ether_header structure defined in
     // ***********************************************************************
+	struct ether_header *link_header;
+	link_header = ( struct ether_header * ) packet;
 
-    	// *******************************************************************
-    	// * If it's an ethernet packet, extract the src/dst address and  
-    	// * find the ethertype value to see what the next layer is.
+	results->newSrcMac( ( uint64_t ) link_header->ether_shost );
+	results->newDstMac( ( uint64_t ) link_header->ether_dhost );
+
+	// log src/dest bytes
+	TRACE << "Processing MAC src address: " << ether_ntoa( ( const ether_addr * ) link_header->ether_shost ) << ENDL;
+	TRACE << "Processing MAC dst address: " << ether_ntoa( ( const ether_addr * ) link_header->ether_dhost ) << ENDL;
+
+	// extract the length or type of the ethernet field
+	uint16_t len_type_bytes = ntohs( link_header->ether_type );
+
+	// log trace len/type
+	TRACE << "Length or type value from ethernet header: " << len_type_bytes << ENDL;
+	
+	// *******************************************************************
+	// * If it's an ethernet packet, extract the src/dst address and  
+	// * find the ethertype value to see what the next layer is.
 	// * 
-	// * If it's not an ethernet packet, count is as "other" and your done
+	// * If it's not an ethernet packet, count is as "other" and you're done
 	// * with this packet.
-    	// *******************************************************************
+	// *******************************************************************
+
+	if(len_type_bytes < ETHERNET_1_THRESH) {
+		results->newOtherLink(len_type_bytes);
+
+		TRACE << "Was not an ethernet packet" << ENDL;
+		// add log for other link
+		return;
+	}
+
+	// add logging for ethernet II
+	TRACE << "Adding new ethernet packet" << ENDL;
+	results->newEthernet( pkthdr->caplen );
 
     // ***********************************************************************
     // * Process the network layer
     // ***********************************************************************
 
-    	// *******************************************************************
+	// *******************************************************************
 	// *  Use ether_type to decide what the next layer is.  You
 	// *  If it's ARP or IPv6 count it and you are done with this packet.
 	// * 
@@ -45,24 +88,119 @@ void pk_processor(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *
 	// * protocol field to see what the next layer is.  
 	// * 
 	// * If it's not ARP, IPv4 or IPv6 count it as otherNetwork.
-    	// *******************************************************************
+	// *******************************************************************
+	bool isIPv4 = false;
+
+	if( len_type_bytes == ARP ) {
+		results->newARP( pkthdr->caplen );
+		TRACE << "Found an ARP packet" << ENDL;
+
+		return;
+	} else if( len_type_bytes == IP_V_6 ) {
+		results->newIPv6( pkthdr->caplen );
+		TRACE << "Found an IPv6 packet" << ENDL;
+
+		return;
+	} else if( len_type_bytes == IP_V_4 ) {
+		isIPv4 = true;
+
+		results->newIPv4( pkthdr->caplen );
+		TRACE << "Found an IPv4 packet" << ENDL;
+
+		struct ip* ip_v4_header;
+		ip_v4_header = ( struct ip * )( packet + IP_OFFSET );
+
+		uint32_t ip_src = ntohl( ip_v4_header->ip_src.s_addr );
+		results->newSrcIPv4( ( uint64_t ) ip_v4_header->ip_src.s_addr );
+		TRACE << "Adding IPv4 src address" << inet_ntoa(ip_v4_header->ip_src) << ENDL;
+
+		uint32_t ip_dst = ntohl( ip_v4_header->ip_dst.s_addr );
+		results->newDstIPv4( ( uint64_t ) ip_v4_header->ip_dst.s_addr );
+		TRACE << "Adding IPv4 dst address" << inet_ntoa(ip_v4_header->ip_dst) << ENDL;
+	} else {
+		results->newOtherNetwork( pkthdr->caplen );
+		TRACE << "Found an otherNetwork packet" << ENDL;
+	}
 
     // ***********************************************************************
     // * Process the transport layer header
     // ***********************************************************************
 
-    	// *******************************************************************
+	// *******************************************************************
 	// * If the packet is an IPv4 packet, then use the Protcol field
 	// * to find out what the next layer is.
 	// * 
 	// * If it's ICMP, count it and you are done with this packet.
 	// *
-	// * If it's UDP or TCP, decode the transport hearder to extract
+	// * If it's UDP or TCP, decode the transport header to extract
 	// * the src/dst ports and TCP flags if needed.
 	// *
 	// * If it's not ICMP, UDP or TCP, count it as otherTransport
-    	// *******************************************************************
+	// *******************************************************************
+	if( isIPv4 ) {
+		struct ip* ip_v4_header;
+		ip_v4_header = ( struct ip * )( packet + IP_OFFSET );
 
+		uint32_t ip_v4_protocol = ( uint32_t )( ip_v4_header->ip_p );
+
+		switch( ip_v4_protocol ) {
+			case ICMP:
+				results->newICMP( pkthdr->caplen );
+				TRACE << "Found an ICMP packet" << ENDL;
+				return;
+			case TCP:
+				// extract the src/dst ports and TCP flags
+				struct tcphdr* tcp_header;
+
+				tcp_header = ( struct tcphdr * ) ( packet + TRANSPORT_LAYER_OFFSET );
+				
+				// add TCP header
+				results->newTCP( pkthdr->caplen );
+				TRACE << "Found a TCP packet" << ENDL;
+
+				// extract src port from TCP header
+				results->newSrcTCP( tcp_header->th_sport );
+				TRACE << "Extracting TCP src port" << tcp_header->th_sport <<  ENDL;
+
+				// extract dst port from TCP header
+				results->newDstTCP( tcp_header->th_dport );
+				TRACE << "Extracting TCP dst port" << tcp_header->th_dport << ENDL;
+
+				// check if SYN bit is set in TCP header
+				if( tcp_header->th_flags & TH_SYN ) {
+					results->incrementSynCount();
+					TRACE << "Incrementing SYN count" << ENDL;
+				}
+
+				// check if FIN bit is set in TCP header
+				if( tcp_header->th_flags & TH_FIN ) {
+					results->incrementFinCount();
+					TRACE << "Incrementing Fin count" << ENDL;
+				}
+
+				break;
+			case UDP:
+				// extract the src/dst ports
+				struct udphdr* udp_header;
+
+				udp_header = ( struct udphdr * ) ( packet + TRANSPORT_LAYER_OFFSET );
+
+				// extract UDP src port from UDP header
+				results->newSrcUDP( udp_header->uh_sport );
+				TRACE << "Extracting UDP src port" << ENDL;
+
+				// extract UDP dst port from UDP header
+				results->newDstUDP( udp_header->uh_dport );
+				TRACE << "Extracting UDP dst port" << ENDL;
+
+				break;
+			default:
+				results->newOtherTransport( pkthdr->caplen );
+				TRACE << "Found an otherTransport packet" << ENDL;
+
+				break;
+		}
+	}
 
     return;
 }
