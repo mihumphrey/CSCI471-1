a09b0303e053b1c0defd4208946e07be07e5bb17
Riley Miller <rmllr000@gmail.com>
2019-12-01 23:37:49 -0700

    release for project 3

--- project3/README.txt
+++ project3/README.txt
@@ -1,1 +1,7 @@
 # Comments and instructions for the grader should go here.
+
+Riley Miller
+
+Just make the file and run the different outputs:
+
+`./packetstats -f testfile.pcap -m -a -t -u -d 3`
--- project3/networkStructures.h
+++ project3/networkStructures.h
@@ -7,8 +7,8 @@
 #ifndef PACKETSTATS_NETWORKSTRUCTURES_H
 #define PACKETSTATS_NETWORKSTRUCTURES_H
 
-#include <arpa/inet.h> // comment out on isengard
-// #include <netinet/ether.h> // uncomment on isengard
+// #include <arpa/inet.h> // comment out on isengard
+#include <netinet/ether.h> // uncomment on isengard
 #include <netinet/if_ether.h>
 
 #include <netinet/ip.h> 
